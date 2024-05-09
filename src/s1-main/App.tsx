34baeecfa3b5ba7aaa898b1f26e0e40782f509f7
Lemeshkov <106582873+Lemeshkov@users.noreply.github.com>
2024-05-09 10:09:34 +0300

    hw6

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,13 +1,14 @@
 import HW1 from "../s2-homeworks/hw01/HW1";
-import HW4 from "../s2-homeworks/hw04/HW4";
-import HW5 from "../s2-homeworks/hw05/HW5";
+import HW2 from "../s2-homeworks/hw02/HW2";
+
 import s from './App.module.css';
 
 function App() {
     return (
         <div className={s.App}>
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
-            <HW1 />
+            {/* <HW1 /> */}
+            <HW2 />
             {/* <HW5 /> */}
         </div>
     )
